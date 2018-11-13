50a9b975f204599220ee820364c82ff7ec5a12b6
Fernando Wijaya <fernando.hendry2@btpn.com>
2018-11-13 11:15:55 +0700

    withLoginData

--- src/screens/Login/LoginScreen.js
+++ src/screens/Login/LoginScreen.js
@@ -1,15 +1,36 @@
 // @flow
 
 import * as React from 'react';
-import {View, Text, StyleSheet} from 'react-native';
+import {View, Text, StyleSheet, TextInput, Button, Alert} from 'react-native';
+import withLoginData from './withLoginData';
 
-type Props = {};
+type Props = {
+  email: string,
+  password: string,
+  onChangeEmail: () => void,
+  onChangePassword: () => void,
+  ShowAlert: () => void,
+};
 
 // eslint-disable-next-line
 function LoginScreen(props: Props) {
+  const {email, password, onChangeEmail, onChangePassword, ShowAlert} = props;
   return (
     <View style={styles.container}>
-      <Text> create your own Login Screen</Text>
+      <Text>Login</Text>
+      <View>
+        <Text>Email</Text>
+        <TextInput value={email} onChangeText={onChangeEmail} />
+      </View>
+      <View>
+        <Text>Password</Text>
+        <TextInput
+          secureTextEntry={true}
+          value={password}
+          onChangeText={onChangePassword}
+        />
+      </View>
+      <Button onPress={ShowAlert} title="Login" />
     </View>
   );
 }
@@ -22,4 +43,4 @@ let styles = StyleSheet.create({
   },
 });
 
-export default LoginScreen;
+export default withLoginData(LoginScreen);
